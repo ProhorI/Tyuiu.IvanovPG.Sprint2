9682f8c8dbcc3edf335447a7e4d70bf576c99a65
Tyuiu.IvanovPG@outlook.com <ivanovprohor491@gmail.com>
2024-10-09 15:59:41 +0500

    Task4

---
+++ Tyuiu.IvanovPG.Sprint2.Task4.V21.Test/DataServiceTest.cs
@@ -0,0 +1,60 @@
+using Tyuiu.IvanovPG.Sprint2.Task4.V21.Lib;
+
+namespace Tyuiu.IvanovPG.Sprint2.Task4.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 10;
+            double y = 5;
+            double res = ds.Calculate(x, y);
+            double wait = 41.822;
+            Assert.AreEqual(wait, res);
+        }
+        
+        
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 15;
+            double res = ds.Calculate(x, y);
+            double wait = 0.446;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
+//using Tyuiu.IvanovPG.Sprint2.Task4.V0.Lib;
+
+//namespace Tyuiu.IvanovPG.Sprint2.Task4.V0.Test
+//{
+//    [TestClass]
+//    public class DataServiceTest
+//    {
+//        [TestMethod]
+//        public void ValidCondition1()
+//        {
+//            DataSevice ds = new DataSevice();
+//            double x = 10;
+//            double y = 5;
+//            double res = ds.Calculate(x, y);
+//            double wait = 3.5;
+//            Assert.AreEqual(wait, res);
+//        }
+//        [TestMethod]
+//        public void ValidCondition2()
+//        {
+//            DataSevice ds = new DataSevice();
+//            double x = 10;
+//            double y = 15;
+//            double res = ds.Calculate(x, y);
+//            double wait = 1.167;
+//            Assert.AreEqual(wait, res);
+//        }
+//    }
+//}
---
+++ Tyuiu.IvanovPG.Sprint2.Task4.V21/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.IvanovPG.Sprint2.Task4.V21.Lib;
+
+
+namespace Tyuiu.IvanovPG.Sprint2.Task4.V21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("**************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                   *");
+            Console.WriteLine("**************************************************************************************");
+
+            Console.WriteLine("Введите значение переменной х: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение переменной y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            double res = ds.Calculate(x, y);
+
+            Console.WriteLine("**************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                         *");
+            Console.WriteLine("**************************************************************************************");
+            Console.WriteLine("Введите значение функции = " + res);
+
+
+            Console.ReadKey();
+        }
+    }
+}
