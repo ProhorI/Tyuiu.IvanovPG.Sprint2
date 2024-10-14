c1f16c747e7794ce6fb68849234374e0c7ba6034
Tyuiu.IvanovPG@outlook.com <ivanovprohor491@gmail.com>
2024-10-14 21:40:32 +0500

    Task5

---
+++ Tyuiu.IvanovPG.Sprint2.Task5.V15.Lib/DataService.cs
@@ -0,0 +1,89 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+
+// ЗАДАНИЕ:
+// По данному номера дня, определить его название.
+
+
+namespace Tyuiu.IvanovPG.Sprint2.Task5.V15.Lib
+{
+    public class DataService : ISprint2Task5V15
+    {
+        public string FindDayName(int k)
+        {
+            string res;
+            switch (k)
+            {
+                case 1:
+                    res = "Понедельник";
+                    break;
+                case 2:
+                    res = "Вторник";
+                    break;
+                case 3:
+                    res = "Среда";
+                    break;
+                case 4:
+                    res = "Четверг";
+                    break;
+                case 5:
+                    res = "Пятница";
+                    break;
+                case 6:
+                    res = "Суббота";
+                    break;
+                case 7:
+                    res = "Воскресенье";
+                    break;
+                default:
+                    throw new ArgumentException($"День должен быть от 1 до 7. Значение {k}");
+            }
+            return res;
+        }
+    }
+}
+
+
+//string res;
+//switch (value)
+//{
+//    case 1:
+//        res = "Январь";
+//        break;
+//    case 2:
+//        res = "Февраль";
+//        break;
+//    case 3:
+//        res = "Март";
+//        break;
+//    case 4:
+//        res = "Апрель";
+//        break;
+//    case 5:
+//        res = "Май";
+//        break;
+//    case 6:
+//        res = "Июнь";
+//        break;
+//    case 7:
+//        res = "Июль";
+//        break;
+//    case 8:
+//        res = "Август";
+//        break;
+//    case 9:
+//        res = "Сентябрь";
+//        break;
+//    case 10:
+//        res = "Октябрь";
+//        break;
+//    case 11:
+//        res = "Ноябрь";
+//        break;
+//    case 12:
+//        res = "Декабрь";
+//        break;
+//    default:
+//        throw new ArgumentException($"Месяц должен быть от 1 до 12. Значение {value}");
+//}
+//return res;
---
+++ Tyuiu.IvanovPG.Sprint2.Task5.V15/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.IvanovPG.Sprint2.Task5.V15.Lib;
+
+namespace Tyuiu.IvanovPG.Sprint2.Task5.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("**************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                   *");
+            Console.WriteLine("**************************************************************************************");
+
+            Console.WriteLine("Введите номер месяца:");
+            int numDay = Convert.ToInt32(Console.ReadLine());
+
+            string res;
+
+            if ((numDay < 1) || (numDay> 7))
+            {
+                res = "Значение введено неверно!";
+            }
+            else
+            {
+                res = "Этот день:" + ds.FindDayName(numDay);
+            }
+
+            Console.WriteLine("**************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                         *");
+            Console.WriteLine("**************************************************************************************");
+
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
