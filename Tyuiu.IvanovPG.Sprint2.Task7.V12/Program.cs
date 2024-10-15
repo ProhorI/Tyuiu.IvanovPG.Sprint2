4868d2b670717ccb6f8d0e7ed595f4b52d153046
Tyuiu.IvanovPG@outlook.com <ivanovprohor491@gmail.com>
2024-10-16 00:09:47 +0500

    Task7

---
+++ Tyuiu.IvanovPG.Sprint2.Task7.V12/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.IvanovPG.Sprint2.Task7.V12.Lib;
+
+namespace Tyuiu.IvanovPG.Sprint2.Task7.V12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                  *");
+            Console.WriteLine("*************************************************************************************");
+
+            Console.WriteLine("Введите значение переменной х:");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение переменной y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            DataService ds = new DataService();
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                        *");
+            Console.WriteLine("*************************************************************************************");
+            if (res)
+            {
+                Console.WriteLine("Точка находится внутри закрашенной области");
+            }
+            else
+            {
+                Console.WriteLine("Точка не находится внутри закрашенной области");
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
