edc28192607ab5fb8071009f551a324fc65d0615
Tyuiu.IvanovPG@outlook.com <ivanovprohor491@gmail.com>
2024-10-15 18:55:33 +0500

    Task6

---
+++ Tyuiu.IvanovPG.Sprint2.Task6.V6.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+
+using Tyuiu.IvanovPG.Sprint2.Task6.V6.Lib;
+
+namespace Tyuiu.IvashkinaKE.Sprint2.Task6.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidFindCardNameAndValue()
+        {
+            DataService ds = new DataService();
+            int m = 4;
+            int k = 10;
+            string res = ds.FindCardNameAndValue(m, k);
+            string wait = "Десятка черви";
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
