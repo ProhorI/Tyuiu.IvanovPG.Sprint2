c5848d7f0aa96540f6bc87157cd93c5bf2389520
Tyuiu.IvanovPG@outlook.com <ivanovprohor491@gmail.com>
2024-10-16 12:24:57 +0500

    Task7

--- Tyuiu.IvanovPG.Sprint2.Task7.V12.Lib/DataService.cs
+++ Tyuiu.IvanovPG.Sprint2.Task7.V12.Lib/DataService.cs
@@ -7,9 +7,10 @@ namespace Tyuiu.IvanovPG.Sprint2.Task7.V12.Lib
         public bool CheckDotInShadedArea(double x, double y)
         {
             bool res;
-            bool z = (y <= Math.Pow(x, 2)) && (y >= 2 - x);
-            bool t = (y >= 0) && y >= Math.Pow(x, 2) && (y <= 2 - x);
-            if (z || t)
+            //bool z = (y <= Math.Pow(x, 2)) && (y >= 2 - x);
+            //bool t = (x <= -2 && x >= 0 && x <= 2 && y >= 0 && y <= 2 && y >= 4);
+            //bool t = (y >= 0) && y >= Math.Pow(x, 2) && (y <= 2 - x);
+            if (y <= Math.Pow(x, 2) && x <= 2 - x && x >= 0 && x <= 2 && y >= 0 && y >= 4)
             {
                 res = true;
             }
@@ -21,3 +22,5 @@ namespace Tyuiu.IvanovPG.Sprint2.Task7.V12.Lib
         }
     }
 }
+//y <= Math.Pow(x, 2) && x <= 2 - x && x >= 0 && x <= 2 && y >= 0 && y >= 4
+//y <= x^2 && x<=-2 && x>= 0 && x <= 2 && y >= 0 && y<= 2 && y>= 4
